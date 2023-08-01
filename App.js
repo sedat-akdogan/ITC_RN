9169b0630233494f75d2396b5e7f7d024fd79b06
Sedat Akdogan <sedat21galaxy@gmail.com>
2023-08-01 09:37:20 +0100

    the project added to the repo

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Class from './src/components/Class';
+import MyComponent from './src/components/MyComponent';
+import AnyClass from './src/components/Practice';
+import MyButton from './src/components/Button';
+
+
+function App() {
+  return (
+    null
+    // <MyButton />
+    // < AnyClass />
+    // <MyComponent />
+    // <Class />
+  );
+}
+
+export default App;
