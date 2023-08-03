dc115cad764dd401c273c23c163b10b33b84a0c6
Sedat Akdogan <sedat21galaxy@gmail.com>
2023-08-03 09:48:30 +0100

    Modal is successfully applied

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import Class from './src/components/Class';
-import MyComponent from './src/components/MyComponent';
-import AnyClass from './src/components/Practice';
-import MyButton from './src/components/Button';
-import MyPractice from './src/components/Practice';
-import TuesdayLesson from './src/components/TuesdayLesson';
-import { SafeAreaView, View } from 'react-native';
-import LevelOne from './src/components/LevelOne';
+// import Class from './src/components/Class';
+// import MyComponent from './src/components/MyComponent';
+// import AnyClass from './src/components/Practice';
+// import MyButton from './src/components/Button';
+// import MyPractice from './src/components/Practice';
+// import TuesdayLesson from './src/components/TuesdayLesson';
+import { SafeAreaView } from 'react-native';
+import MyModal from './src/components/MyModal';
+// import Pizza from './src/components/Pizza';
+// import Cafe from './src/components/Cafe';
 
 
 function App() {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: "red", flexDirection: "row"}}>
+    <SafeAreaView >
+      <MyModal />
+      {/* <Pizza /> */}
       {/* <MyPractice /> */}
       {/* <TuesdayLesson /> */}
-      <LevelOne />
-      <LevelOne />
-      <LevelOne />
+      {/* <Cafe /> */}
       {/* // <MyButton />
-    // < AnyClass />
-    // <MyComponent />
-    // <Class /> */}
+        < AnyClass />
+        <MyComponent />
+        <Class /> */}
     </SafeAreaView>
   );
 }
